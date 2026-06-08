37162478206f55022bd38e638d001d0c28c089a9
zcasparco <57662585+zcasparco@users.noreply.github.com>
2026-06-08 09:03:14 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(name='swot_analysis',
+      description='Tools for my postdoc at UHH',
+      url='https://github.com/zcasparco/SWOT_UHH',
+      packages=['swot_analysis'])
